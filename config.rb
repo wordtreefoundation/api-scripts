16e4791a1223ff821e0e572e7ea7e9c5ab7b12ca
Duane Johnson <duane.johnson@gmail.com>
2014-08-10 15:43:36 -0600

    Add app

--- config.rb
+++ config.rb
@@ -1,7 +1,8 @@
+this_dir = File.dirname(__FILE__)
 CONFIG = {
-  :host    => ENV['RDB_HOST']  || 'localhost', 
-  :port    => (ENV['RDB_PORT'] || '28015').to_i,
-  :db      => ENV['RDB_DB']    || 'research',
-  :library => ENV['LIBRARY']   || 'library',
-  :logdir  => ENV['LOGDIR']    || File.join(File.dirname(__FILE__), 'logs')
+  :host          => ENV['RDB_HOST']  || 'localhost', 
+  :port          => (ENV['RDB_PORT'] || '28015').to_i,
+  :db            => ENV['RDB_DB']    || 'research',
+  :library       => ENV['LIBRARY']   || 'library',
+  :logdir        => ENV['LOGDIR']    || File.join(this_dir, 'logs')
 }
--- rethinkdb_job.rb
+++ rethinkdb_job.rb
@@ -1,9 +1,8 @@
 require 'rethinkdb'
 
 class RethinkDBJob
-  def initialize(host, port, db, logdir, table="jobs")
+  def initialize(host, port, db, table="jobs")
     @r = RethinkDB::RQL.new
-    @logdir = logdir
 
     setup_rethinkdb(host, port, db, table)
 
@@ -49,17 +48,5 @@ class RethinkDBJob
 
   def set_timestamp(job_id, column, time=Time.now)
     @r.table(@table).get(job_id).update(column => time).run(@rdb)
-  end
-
-  def get(job_id)
-    @r.table(@table).get(job_id).run(@rdb)
-  end
-
-  def logfile(job_id)
-    File.join(@logdir, "job_#{job_id}.log")
-  end
-
-  def tail(job_id, lines=10)
-    `tail -#{Integer(lines).to_s} #{logfile(job_id)} 2>&1`
   end
 end
